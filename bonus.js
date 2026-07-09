3d24a57ff1413a0a097e81ca3c6f634dbddb9b6a
RazanHamza <Kabbaskhaled116@gmail.com>
2026-07-09 19:26:28 +0300

    Add files via upload

---
+++ bonus.js
@@ -0,0 +1,24 @@
+function createCounter(init) {
+
+    let temp = init;
+
+    return {
+        increment: function () {
+            return ++init;
+        },
+
+        decrement: function () {
+            return --init;
+        },
+
+        reset: function () {
+            init = temp;
+            return init;
+        }
+    };
+}
+
+const counter = createCounter(8);
+console.log(counter.increment());
+console.log(counter.reset());     
+console.log(counter.decrement());
