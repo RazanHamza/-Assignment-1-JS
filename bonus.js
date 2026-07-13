795df95dcb8a299ba0a925a9b285b3163fdf1fda
RazanHamza <Kabbaskhaled116@gmail.com>
2026-07-14 00:56:47 +0300

    Add decrement method call in bonus.js
    Added decrement method call to the counter.

--- bonus.js
+++ bonus.js
@@ -1,23 +1,18 @@
 function createCounter(init) {
-
     let temp = init;
-
     return {
         increment: function () {
             return ++init;
         },
-
         decrement: function () {
             return --init;
         },
-
         reset: function () {
             init = temp;
             return init;
         }
     };
 }
-
 const counter = createCounter(8);
 console.log(counter.increment());
 console.log(counter.reset());     
